4b461dca3bd9b35a26f8fc891a4ddad10bad685a
LTDat5 <88955236+LTDat03@users.noreply.github.com>
2024-08-21 13:03:51 +0700

    add: Exercise 1 of Developer Skills & Editor Setup

---
+++ Developer_Skills_&_Editor_Setup/main.js
@@ -0,0 +1,18 @@
+// Developer Skills & Editor Setup
+
+// Coding Challenge #1
+console.log("Exercise #1");
+printForecast = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`${arr[i]}°C in ${i + 1} days...`);
+    }
+}
+
+Data1 = [17, 21, 23];
+
+Data2 = [12, 5, -5, 0, 4];
+
+printForecast(Data1);
+
+printForecast(Data2);
+
