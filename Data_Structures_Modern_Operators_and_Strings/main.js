a1891b8835c5910cde6af4b72d2091ae12d9093b
LTDat5 <88955236+LTDat03@users.noreply.github.com>
2024-08-25 14:13:05 +0700

    add: Coding Challenge 3 of Data Structures, Modern Operators and Strings

--- Data_Structures_Modern_Operators_and_Strings/main.js
+++ Data_Structures_Modern_Operators_and_Strings/main.js
@@ -95,3 +95,37 @@ for (player of game.scored){
 }
 
 console.log(scorers);
+
+// Coding Challenge #3
+
+console.log('Coding Challenge #3');
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+  ]);
+
+const events = {...new Set(gameEvents.values())};
+
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+for ([min, event] of gameEvents){
+  console.log(`[${min <= 45 ? 'FIRST HALF' : 'SECOND HALF'}] ${min}: ${event}`);
+}
+
+
+
